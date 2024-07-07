9056dedf58040a3e5542682a306e3c690286d676
Hatzilu <itamar246@gmail.com>
2024-07-07 22:38:21 +0300

    Input box isw working

---
+++ scripts/KeyboardInput.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+@onready var input = $LineEdit
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	input.grab_focus()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_line_edit_text_submitted(new_text):
+	print(new_text)
+	input.clear()
+	pass # Replace with function body.
